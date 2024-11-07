1799acc9e35e3e9a88593f719d43bb154687dac1
“masum182” <masum.mollah@webable.digital>
2024-11-07 16:38:30 +0600

    added fix issus

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View className="p-4 bg-slate-600">
+      <Text className="text-white capitalize">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero unde, blanditiis, neque obcaecati ullam quaerat id aspernatur aperiam dolore harum fugit fuga dignissimos, sapiente atque reprehenderit accusamus non voluptatibus quia!</Text>
+     
+    </View>
+  )
+}
+
+export default App
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'nativewind/babel', 
+    ['@babel/plugin-transform-private-methods', { loose: true }]
+  ],
 };
+
